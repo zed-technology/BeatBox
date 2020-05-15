26465e22f49ffd413b4409bbdd2230b266129b2c
Denis Klimenko <suncoin@gmail.com>
2020-05-15 16:23:02 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
+}
